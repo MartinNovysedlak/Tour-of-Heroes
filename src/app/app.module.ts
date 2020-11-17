9d34221c468a4de9279f3478250bc353f09f78ad
MartinNovysedlak <martinnovysedlak48@gmail.com>
2020-11-17 18:36:25 +0100

    odstranenie itemov

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,13 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { ItemsComponent } from './items/items.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     ItemsComponent,
-    HeroDetailComponent,
-    ItemDetailComponent  
+    HeroDetailComponent, 
   ],
   imports: [
     BrowserModule,
